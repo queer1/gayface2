aed6e31c05737526978f03605c064a566eda36cb
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-28 20:30:32 -0400

    going to leave it here and try to add another gallery controller. mid-level check point

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :blurb, :city, :first_name, :image_url, :last_name, :occupation, :sexuality, :state
+
+  validates :city, :first_name, :image_url, :last_name, :occupation, :sexuality, :state, presence: true
+
+  validates :image_url, allow_blank: true, format: {
+  with: %r{\.(gif|jpg|png)\Z}i,
+  message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  
 end
